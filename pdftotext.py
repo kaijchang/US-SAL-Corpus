4d7c78fafd71dd5e73b952b7b18c9db272f1b9c9
Kai Chang <kaijchang@gmail.com>
2024-03-23 11:05:39 -0400

    black

--- pdftotext.py
+++ pdftotext.py
@@ -2,19 +2,19 @@ import os
 import subprocess
 import re
 
-PDFS_DIR = 'pdfs'
-TEXT_DIR = 'text'
+PDFS_DIR = "pdfs"
+TEXT_DIR = "text"
 
 os.makedirs(TEXT_DIR, exist_ok=True)
 
 for filename in os.listdir(PDFS_DIR):
     pdf_path = os.path.join(PDFS_DIR, filename)
-    text_path = os.path.join(TEXT_DIR, f'{os.path.splitext(filename)[0]}.txt')
-    
+    text_path = os.path.join(TEXT_DIR, f"{os.path.splitext(filename)[0]}.txt")
+
     if not os.path.exists(text_path):
-        print(f'Converting {pdf_path} to {text_path}')
+        print(f"Converting {pdf_path} to {text_path}")
         options = []
-        subprocess.run(['pdftotext', *options, pdf_path, text_path])
+        subprocess.run(["pdftotext", *options, pdf_path, text_path])
 
     else:
-        print(f'File {text_path} already exists')
+        print(f"File {text_path} already exists")
